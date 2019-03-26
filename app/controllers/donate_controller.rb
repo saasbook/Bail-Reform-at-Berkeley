81c1ca6ee8002e264312a342b8b2331389ca0e4b
Autumn Wozniak <autumnwozniak@berkeley.edu>
2019-03-25 19:20:45 -0700

    Added Stripe

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -1,4 +1,28 @@
 class DonateController < ApplicationController
   def new
   end
+
+  def create
+    Stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+    token = params[:stripeToken]
+    amt = params[:amount]
+
+    begin
+      charge = Stripe::Charge.create({
+          amount: amt,
+          currency: 'usd',
+          description: 'Bail Reform at Berkely Donation',
+          source: token,
+      })
+      redirect_to successful_donation_path
+    rescue
+      redirect_to failed_donation_path
+    end
+  end
+
+  def success
+  end
+
+  def fail
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'donate/new' => 'donate#new', :as => 'new_donation'
+  get 'donate/success' => 'donate#success', :as => 'successful_donation'
+  get 'donate/error' => 'donate#fail', :as => 'failed_donation'
+  get 'donate/:stripeToken/:amount' => 'donate#create', :as => 'create_donation'
   get 'home' => 'home#index',   :as => 'home'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
